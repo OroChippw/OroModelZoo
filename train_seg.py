471860981b4e433e43c272d5fea39e963abf912d
OroChippw <OroChippw@163.com>
2022-12-30 11:02:47 +0800

    feat:func add weights_init and update train_seg

--- train_seg.py
+++ train_seg.py
@@ -1,6 +1,8 @@
+import time
+import yaml
 import argparse
 import os , os.path as osp
-import time
+import numpy as np
 from tqdm import tqdm
 
 import torch
@@ -12,7 +14,8 @@ from torch.utils.data import DataLoader , Dataset
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.tensorboard import SummaryWriter
 
-from utils import (mkdir_or_exist , init_dist_pytorch , get_dist_info , build_multi_process)
+from utils import (mkdir_or_exist , init_dist_pytorch , get_dist_info , 
+    build_multi_process  , weights_init)
 
 
 
@@ -20,13 +23,13 @@ def args_parse():
     parser = argparse.ArgumentParser(description="")
     # Model trainning settings
     parser.add_argument("--config",type=str,help="train config file path")
-    parser.add_argument("--checkpoint",type=str,default="",help="initial weights path")
-    parser.add_argument("--start_epoches",type=int,default=0,help="start training epoches")
-    parser.add_argument("--num_epoches",type=int,default=2000,help="total training epoches")
-    parser.add_argument("--batch-size","-b",type=int,default=64,help="")
+    # parser.add_argument("--checkpoint",type=str,help="initial weights path")
+    # parser.add_argument("--start_epoches",type=int,default=0,help="start training epoches")
+    # parser.add_argument("--num_epoches",type=int,default=2000,help="total training epoches")
+    # parser.add_argument("--batch-size","-b",type=int,default=64,help="")
     parser.add_argument("--fp16",type=bool,default=False,action="store_true", help="")
-    parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
-    parser.add_argument("--work-dir",type=str,default="",help="")
+    # parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
+    # parser.add_argument("--work-dir",type=str,default="",help="")
     # Distributed Data Parallel settings
     parser.add_argument("--device",default="",help="cuda device or cpu")
     parser.add_argument("--num_worker",type=int,default=4,help="")
@@ -42,6 +45,15 @@ def args_parse():
 def main():
     args = args_parse()
 
+    if args.config is not None:
+        assert osp.isfile(args.config) and args.config.endswith('.yaml') , \
+            f"args.config should be a yaml file , Instead of {osp.splitext(args.config)[-1]}"
+        with open(args.config , 'r' , errors='ignore') as f:
+            temp_ = yaml.load(f.read())
+            print(temp_)
+        for k , v in temp_.item():
+            setattr(args , k , v)
+        
     if args.work_dir is None :
         args.work_dir = osp.join('./work_dirs' , osp.splitext(osp.basename(args.config))[0])
     mkdir_or_exist(args.work_dir)
@@ -64,34 +76,79 @@ def main():
     # build multi-process settings
     build_multi_process(args.gpu_)
     
+    # build datasets
+
+
     # SyncBN is not support in DP mode
     if distributed_:
         train_sampler = DistributedSampler(dataset="" , shuffle=True)
+        val_sampler = DistributedSampler(dataset="" , shuffle=False)
 
     train_dataloader = DataLoader("" , batch_size=args.batch_size)
 
+    # Init Automatic mixed precision GradScaler
     if args.fp16:
+        # torch.cuda.amp provides users with a more convenient mixed-precision training mechanism.
+        # Users do not need to manually convert the model parameter dtype, amp will automatically select the appropriate numerical precision for the operator
+        # For the problem of FP16 gradient value overflow during backpropagation, amp provides a gradient scaling operation, 
+        # and before the optimizer updates the parameters, it will automatically unscale the gradient, so there will be no hyperparameters for model optimization. 
         from torch.cuda.amp import GradScaler as GradScaler
-        scaler = GradScaler()
+        scaler_ = GradScaler()
     else :
-        scaler = None
+        scaler_ = None
 
 
     # Init tensorboard summarywriter
     writer_ = SummaryWriter('./logs')
 
     model_ = None
+
+    if args.checkpoint is None:
+        weights_init(model_)
+    else : 
+        # Load according to the Key of the pre-trained weight and the Key of the model
+        model_dict_ = model_.state_dict()
+        pretrain_dict_ = torch.load(args.checkpoint , map_location=args.device)
+        success_load , fail_load , temp_dict = [] , [] , {}
+        for k , v in pretrain_dict_.items():
+            if k in model_dict_.keys() and np.shape(model_dict_[k]) == np.shape(v):
+                temp_dict[k] = v
+                success_load.append(k)
+            else : 
+                fail_load.append(k)
+        model_dict_.update(temp_dict)
+        model_.load_state_dict(model_dict_)
+        if rank == 0:
+            print("Successfully Load Key : " , str(success_load))
+            print("Fail Load Key : " , str(fail_load))
+        
+    if distributed_:
+        torch.nn.SyncBatchNorm.convert_sync_batchnorm(model_)
+
     # loss function
-    crit_ = nn.CrossEntropyLoss()
+    criterion_ = nn.CrossEntropyLoss()
     # optimizer 
-    optimizer_ = optim.Adam()
+    if args.TRAIN.optimizer == 'Adam':
+        optimizer_ = optim.Adam(model_.parameters() , lr=args.TRAIN.LR)
+    elif args.TRAIN.optimizer == 'SGD':
+        optimizer_ = optim.SGD(model_.parameters() , lr=args.TRAIN.LR , momentum=0.9 , weight_decay=0.0001)
+    if args.TRAIN.multistep_lr:
+        lr_scheduler_ = optim.lr_scheduler.MultiStepLR(optimizer=optimizer_ , milestones=args.TRAIN.LR_STEP , 
+                            gamma=args.TRAIN.LR_FACTOR)
+
     metric_ = None
+
     for epoch in range(args.start_epoches , args.num_epoches):
         model_.train()
         optimizer_.zero_grad()
-        loss_ = crit_()
-        loss_.backward()
-        optimizer_.step()
+        loss_ = criterion_()
+        if scaler_ is not None : 
+            scaler_.scale(loss=loss_).backward()
+            scaler_.step(optimizer=optimizer_)
+            scaler_.update()
+        else : 
+            loss_.backward()
+            optimizer_.step()
         writer_.add_scalar("loss function" , loss_.item() , epoch)
         
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -3,6 +3,7 @@ from .paths import mkdir_or_exist
 from .dist_utils import init_dist_pytorch , get_dist_info
 from .logger import print_logger , get_logger
 from .build_env import build_multi_process
+from .weights_init import weights_init
 
 __all__ = ["Registry" , "build_from_cfg" , "mkdir_or_exist" , "init_dist_pytorch" , "get_dist_info" ,
-            "print_logger" , "get_logger" , "build_multi_process"]
+            "print_logger" , "get_logger" , "build_multi_process" , "weights_init"]
